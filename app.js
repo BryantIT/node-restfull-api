688fbf4f1bc562b7a4db73b596a21d6a7c71a9eb
Bryant Richards <dragonseyeimages@gmail.com>
2020-08-17 20:48:36 -0500

    set up db connection

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
-
+const mongoose = require('mongoose')
 const app = express()
+require('dotenv/config')
 
 //ROUTES
 app.get('/', (req, res) => {
@@ -10,5 +11,13 @@ app.get('/', (req, res) => {
 app.get('/posts', (req, res) => {
   res.send('Hello posts')
 })
+
+//Connect to DB
+mongoose.connect(
+  process.env.DB_CONNECTION,
+  { useNewUrlParser: true, useUnifiedTopology: true  },
+   () => console.log('connected to DB')
+)
+
 
 app.listen(3000)
